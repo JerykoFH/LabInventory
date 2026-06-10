62a87b3e4944804af0336b18a4fcd52245c2ab2f
Jeryko <gahas809@gmail.com>
2026-06-11 00:31:37 +0700

    buat nama ruangan di admin jadi unique

--- backend/src/models/Room.js
+++ backend/src/models/Room.js
@@ -5,6 +5,7 @@ const roomSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Room name is required'],
+        unique: true,
         trim: true,
     },
     code: {
